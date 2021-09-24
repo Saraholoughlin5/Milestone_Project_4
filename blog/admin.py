769a6f2ba92bd246e25c9edfedaa26c2595aa360
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-09-24 20:41:32 +0000

    Add and register blog views, urls and forms

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import BlogPost, BlogComment
 
-# Register your models here.
+
+class BlogPostAdmin(admin.ModelAdmin):
+    """
+    Setup Blog Post section of Admin Panel
+    """
+
+    list_display = (
+        'title',
+        'created_on',
+        'image',
+    )
+
+    search_fields = (
+        'title',
+        'body_text',
+    )
+
+
+class BlogCommentAdmin(admin.ModelAdmin):
+    """
+    Setup Blog Comment section of Admin Panel
+    """
+    list_display = (
+        'user',
+        'blog_post',
+        'comment',
+        'created_on',
+    )
+
+    search_fields = (
+        'user',
+        'comment',
+    )
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(BlogComment, BlogCommentAdmin)
