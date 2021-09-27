96a2a23a234635a01da892efb01aa35eeedeb716
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-09-27 10:35:23 +0000

    Remove secret key and set debug

--- indulgem/settings.py
+++ indulgem/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n%%!42(z6vm%m0mt=t*z$)vt02rder%k@shly#a_l#p7!rlu(8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['indulgem-mp4.herokuapp.com', 'localhost']
 
