1e7a37340cc3ff67c71280dec1848f0eed631ed2
Saraholoughlin5 <saraholoughlin5@gmail.com>
2021-09-27 11:21:07 +0000

    Set up static file collection in AWS

---
+++ custom_storages.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+# Cloud storage location for static files using AWS
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+
+
+# Cloud storage location for media files using AWS
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
+    
